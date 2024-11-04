635b4cd3df34f7a7609e2463f27cee81dd0bf804
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2024-11-04 23:20:10 +0900

    Company controller 로그 추가

--- project/src/main/java/com/mandf/project/makeandfollow/company/controller/CompanyController.java
+++ project/src/main/java/com/mandf/project/makeandfollow/company/controller/CompanyController.java
@@ -6,6 +6,7 @@ import com.mandf.project.makeandfollow.company.dto.CompanyInsertRequestDto;
 import com.mandf.project.makeandfollow.company.dto.CompanyUpdateRequestDto;
 import com.mandf.project.makeandfollow.company.service.CompanyService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,31 +14,32 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/company")
 @RequiredArgsConstructor
+@Slf4j
 public class CompanyController {
 
     private final CompanyService companyService;
 
     @PostMapping("/findbyid")
     public ResponseEntity<?> findCompanyByCode(@RequestBody CompanyFindByCodeRequestDto companyFindByCodeRequestDto){
-
+        log.info("FindCompanyByCode :: RequestBody[{}]", companyFindByCodeRequestDto);
         return new ResponseEntity<>(companyService.findCompanyByCode(companyFindByCodeRequestDto), HttpStatus.OK);
     }
 
     @PostMapping("/findbyuuid")
     public ResponseEntity<?> findCompanyByUUID(@RequestBody CompanyFindByUUIDRequestDto companyFindByUUIDRequestDto){
-
+        log.info("FindCompanyByUUID :: RequestBody[{}]", companyFindByUUIDRequestDto);
         return new ResponseEntity<>(companyService.findCompanyByUUID(companyFindByUUIDRequestDto), HttpStatus.OK);
     }
 
     @PostMapping("/insert")
     public ResponseEntity<?> insertCompany(@RequestBody CompanyInsertRequestDto companyInsertRequestDto) throws Exception {
-
+        log.info("InsertCompany :: RequestBody[{}]", companyInsertRequestDto);
         return new ResponseEntity<>(companyService.insertCompany(companyInsertRequestDto), HttpStatus.OK);
     }
 
     @PostMapping("/delete")
     public ResponseEntity<?> updateCompanyStatus(@RequestBody CompanyUpdateRequestDto companyUpdateRequestDto) throws Exception {
-
+        log.info("UpdateCompanyStatus :: RequestBody[{}]", companyUpdateRequestDto);
         return new ResponseEntity<>(companyService.updateCompany(companyUpdateRequestDto),HttpStatus.OK);
     }
 
