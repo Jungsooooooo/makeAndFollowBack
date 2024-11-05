a41d41996743cbdc50ed9c58a9067b051ac190b1
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2024-11-05 23:20:36 +0900

    User Variable controller log 추가

--- project/src/main/java/com/mandf/project/makeandfollow/user/controller/UserController.java
+++ project/src/main/java/com/mandf/project/makeandfollow/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.mandf.project.makeandfollow.user.dto.UserJoinRequestDto;
 import com.mandf.project.makeandfollow.user.dto.UserLoginRequestDto;
 import com.mandf.project.makeandfollow.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,19 +17,20 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userService;
 
     @PostMapping("/join")
-    public ResponseEntity<?> userJoin(@RequestBody @Valid UserJoinRequestDto userJoinRequestDto) throws Exception {
-
+    public ResponseEntity<?> insertUser(@RequestBody @Valid UserJoinRequestDto userJoinRequestDto) throws Exception {
+        log.info("InsertUser :: RequestBody[{}]", userJoinRequestDto);
         return new ResponseEntity<>(userService.insertUser(userJoinRequestDto), HttpStatus.OK);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> userLogin(@RequestBody @Valid UserLoginRequestDto userLoginRequestDto) throws Exception {
-
+    public ResponseEntity<?> loginUser(@RequestBody @Valid UserLoginRequestDto userLoginRequestDto) throws Exception {
+        log.info("LoginUser :: RequestBody[{}]", userLoginRequestDto);
         return new ResponseEntity<>(userService.userLogin(userLoginRequestDto), HttpStatus.OK);
     }
 
--- project/src/main/java/com/mandf/project/makeandfollow/variable/controller/VariableController.java
+++ project/src/main/java/com/mandf/project/makeandfollow/variable/controller/VariableController.java
@@ -4,6 +4,7 @@ import com.mandf.project.makeandfollow.variable.dto.VariableRequestDto;
 import com.mandf.project.makeandfollow.variable.dto.VariableWithCompanyUserUidRequestDto;
 import com.mandf.project.makeandfollow.variable.service.VariableService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,29 +15,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/test")
 @RequiredArgsConstructor
+@Slf4j
 public class VariableController {
 
     private final VariableService variableService;
 
-    @PostMapping("/success")
-    public String testAPi() {
-        return "success";
-    }
-
     @PostMapping("/findAll")
     public ResponseEntity<?> findVariableAll() {
+        log.info("FindCompanyByCode ::");
         return new ResponseEntity<>(variableService.getAllVariables(), HttpStatus.OK);
     }
 
     @PostMapping("/insert-variable")
     public ResponseEntity<?> insertVariable(@RequestBody VariableRequestDto variableRequestDto) throws Exception {
-
+        log.info("insertVariable :: RequestBody[{}]", variableRequestDto);
         return new ResponseEntity<>(variableService.insertVariable(variableRequestDto), HttpStatus.OK);
     }
 
     @PostMapping("/insert-with-variable-company-user-uid")
     public ResponseEntity<?> insertVariableWithCompanyUserUid(@RequestBody VariableWithCompanyUserUidRequestDto variableWithCompanyUserUidRequestDto) throws Exception {
-
+        log.info("InsertVariableWithCompanyUserUid :: RequestBody[{}]", variableWithCompanyUserUidRequestDto);
         return new ResponseEntity<>(variableService.insertVariableWithCompanyUserUid(variableWithCompanyUserUidRequestDto), HttpStatus.OK);
     }
 }
